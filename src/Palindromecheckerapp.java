e8c2d9f42eb1c06d99d73242fdec2cf8412bdea3
athreyagame2-hacks <athreyagame2@gmail.com>
2026-02-26 10:32:16 +0530

    UC13: Performance Comparison

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,28 +1,26 @@
 public class Palindromecheckerapp {
     public static void main(String[] args){
         String input = "level";
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean isPalindrome = strategy.check(input);
+
+        long start = System.nanoTime();
+
+        boolean isPalindrome = true;
+        int i = 0, j = input.length() - 1;
+        while (i < j) {
+            if (input.charAt(i) != input.charAt(j)) {
+                isPalindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        long end = System.nanoTime();
+        long time = end - start;
+
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-    }
-}
+        System.out.println("Execution Time : " + time + " ns");
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
     }
 }
