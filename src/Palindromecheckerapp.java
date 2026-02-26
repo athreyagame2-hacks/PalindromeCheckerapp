bd3ccefcca2f6f00f32ebcb73161ca9aeda4191a
athreyagame2-hacks <athreyagame2@gmail.com>
2026-02-26 10:03:41 +0530

    UC8: Linked List Based Palindrome Checker

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,18 +1,18 @@
 import java.util.*;
 public class Palindromecheckerapp {
     public static void main(String[] args){
-        String input = "refer";
+        String input = "level";
 
-        Deque<Character> deque = new ArrayDeque<>();
+        LinkedList<Character> list = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
-            deque.add(c);
+            list.add(c);
         }
 
         boolean isPalindrome = true;
 
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
                 isPalindrome = false;
                 break;
             }
