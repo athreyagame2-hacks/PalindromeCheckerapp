cec671cd238da2b3f4f16d2fcca9c76d7f7e7ecc
athreyagame2-hacks <athreyagame2@gmail.com>
2026-02-26 08:48:55 +0530

    UC1: Welcome statement is created

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,2 +1,7 @@
 public class Palindromecheckerapp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the palindrome checker management system");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
 }
